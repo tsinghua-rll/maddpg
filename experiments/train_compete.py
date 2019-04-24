57c34954d89cb0f2f19f778fd6565f6d25f70324
Jeasine Ma <jeasinema@gmail.com>
2019-04-24 17:20:12 +0800

    ...

--- experiments/train_compete.py
+++ experiments/train_compete.py
@@ -15,7 +15,7 @@ def parse_args():
     parser = argparse.ArgumentParser("Reinforcement Learning experiments for multiagent environments")
     # Environment
     parser.add_argument("--scenario", type=str, default="simple", help="name of the scenario script")
-    parser.add_argument("--max-episode-len", type=int, default=25, help="maximum episode length")
+    parser.add_argument("--max-episode-len", type=int, default=1000, help="maximum episode length")
     parser.add_argument("--num-episodes", type=int, default=6000, help="number of episodes")
     parser.add_argument("--num-adversaries", type=int, default=1, help="number of adversaries")
     parser.add_argument("--good-policy", type=str, default="maddpg", help="policy for good agents")
